aee8079ff2ac8321326fdabecad9643cf438525d
Charles M <charles@greybeard.io>
2023-09-19 08:36:03 -0500

    Removing docker-compose in favor of docker compose that is native to the docker binary

--- showman.sh
+++ showman.sh
@@ -11,7 +11,8 @@ log_path="$base_dir/log"
 log_file="$log_path/messages"
 tmp_file="$log_path/workfile.tmp"
 log_date=$(date '+%Y-%m-%d %H:%M')
-dc_exec="/usr/local/bin/docker-compose -f $base_dir/compose/showman.yaml"
+## dc_exec="/usr/local/bin/docker-compose -f $base_dir/compose/showman.yaml"
+dc_exec="/usr/bin/docker compose -f $base_dir/compose/showman.yaml"
 
 ###########################################
 
@@ -112,8 +113,8 @@ function showman_install () {
 	apt-get -y install docker-ce docker-ce-cli containerd.io
 
 	### Install docker-compose
-	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
+	### curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	### chmod +x /usr/local/bin/docker-compose
 
 	### Install user
 	useradd \
