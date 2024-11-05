e0b1c0838c874c0b601b89584497a767ff361f90
Charles M <charlesm@graybeard.io>
2024-11-05 15:23:53 -0600

    Further reduction of the script to an install only solution.

--- showman-ng.sh
+++ showman-ng.sh
@@ -6,124 +6,24 @@ base_dir='/opt/showman' ## full path to the mount point for persistent data
 
 ## global variables #######################
 
-log_path="$base_dir/log"
-log_file="$log_path/messages"
-tmp_file="$log_path/workfile.tmp"
 dc_exec="/usr/bin/docker compose -f $base_dir/compose/showman.yaml"
 
 ###########################################
 
-function log_action() {
-  local log_date=$(date '+%Y-%m-%d %H:%M')
-  echo "$log_date ** executed showman $1" >> "$log_file"
-}
-
-function create_systemd_service() {
-  local sys_path='/etc/systemd/system'
-  local timer='showman-check.timer'
-  local service='showman-check.service'
-
-  # Create timer
-  {
-    echo "[Unit]"
-    echo "Description=Daily check for docker image updates (installed by showman)"
-    echo "Requires=showman-check.service"
-    echo ""
-    echo "[Timer]"
-    echo "Unit=showman-check.service"
-    echo "OnCalendar=*-*-* 4:00:00"
-    echo ""
-    echo "[Install]"
-    echo "WantedBy=timers.target"
-  } > "$sys_path/$timer"
-
-  # Create service
-  {
-    echo "[Unit]"
-    echo "Description=Checks for docker image updates (installed by showman)"
-    echo "Wants=showman-check.timer"
-    echo ""
-    echo "[Service]"
-    echo "Type=oneshot"
-    echo "ExecStart=/bin/bash $base_dir/bin/showman.sh update"
-    echo ""
-    echo "[Install]"
-    echo "WantedBy=multi-user.target"
-  } > "$sys_path/$service"
-
-  systemctl daemon-reload
-  systemctl start "$timer"
-  systemctl enable "$timer"
-}
-
-function schedule_cron_jobs() {
-  (crontab -l 2>/dev/null; echo "0 4 * * * /bin/bash $base_dir/bin/showman.sh update >/dev/null 2>&1") | crontab -
-  (crontab -l 2>/dev/null; echo "0 0 1 * * /bin/bash $base_dir/bin/showman.sh rotate >/dev/null 2>&1") | crontab -
-}
-
-function make_routine() {
-  . /etc/os-release
-  case "$ID" in
-    arch)
-      create_systemd_service
-      ;;
-    debian)
-      schedule_cron_jobs
-      ;;
-  esac
-}
-
-function showman_up() {
-  $dc_exec up -d
-  log_action "up"
-}
-
-function showman_down() {
-  $dc_exec down
-  log_action "down"
-}
-
-function showman_destroy() {
-  $dc_exec --rmi all down
-  log_action "destroy"
-}
-
 function showman_start() {
   $dc_exec start
-  log_action "start"
-}
-
-function showman_stop() {
-  $dc_exec stop
-  log_action "stop"
-}
-
-function get_user_choice() {
-  local prompt="$1"
-  local default="$2"
-  local choice
-
-  printf "$prompt"
-  read -r choice
-  choice=${choice:-$default}
-  echo "${choice,,}" | cut -c1  # Return first character, lowercase
 }
 
 function showman_install() {
   ## figure out which base distro we're running
   . /etc/os-release
 
-  printf "\n\tStarting Showman install...\n\n"
-
   ## function variables
   local user='showman'
   local group='showman'
-  local all_directories=('bin' 'config' 'compose' )
-  #local all_directories=('bin' 'config' 'content' 'factory' 'compose' )
+  local all_directories=('bin' 'config' 'compose' 'content' 'factory' )
 
   mkdir -p "$base_dir" || { echo "Failed to create base directory"; exit 1; }
-  cp -a "$(dirname "$0")" "$base_dir/bin/"
-  chmod 700 "$base_dir/bin/showman.sh"
 
   ## Install Docker based on the OS
   case "$ID" in
@@ -148,7 +48,7 @@ function showman_install() {
 
   # Create necessary directories
   for directory in "${all_directories[@]}"; do
-    mkdir -p "$base_dir/$directory" && chown -R "$user:$group" "$base_dir/$directory"
+    mkdir -p "$base_dir/$directory" 
   done
 
   cp ./showman-ng.yaml "$base_dir/compose/showman.yaml"
@@ -165,9 +65,9 @@ function showman_install() {
          -e "s/DISCORD_ID/$DISCORD_ID/g" \
          -e "s/DISCORD_TOKEN/$DISCORD_TOKEN/g" \
          "$base_dir/compose/showman.yaml"
+  
+  chown -R $user_id:$group_id $base_dir
 
-  #make_routine
-  log_action "install"
   showman_up
 }
 
@@ -179,39 +79,9 @@ function compose_up() {
   $dc_exec up -d
 }
 
-function log_rotate() {
-  for ((i=6; i>0; i--)); do
-    [[ -f $log_path/messages.$i ]] && mv "$log_path/messages.$i" "$log_path/messages.$((i+1))"
-  done
-  [[ -f $log_path/messages ]] && mv "$log_path/messages" "$log_path/messages.1"
-  log_action "rotate"
-  echo "$(date '+%Y-%m-%d %H:%M') ~~~~ rotated showman log files" >> "$log_file"
-}
-
-function update_log() {
-  if grep -qi 'recreat' "$tmp_file"; then
-    /usr/bin/docker image prune -f -a
-    log_action "update"
-    echo "$(date '+%Y-%m-%d %H:%M') ++ update has been found" >> "$log_file"
-
-    while read -r service; do
-      echo "$(date '+%Y-%m-%d %H:%M') ++++ $service container updated" >> "$log_file"
-    done < <(grep -i 'recreat' "$tmp_file" | cut -d' ' -f3 | sort -u)
-  else
-    echo "$(date '+%Y-%m-%d %H:%M') ---- no updates found" >> "$log_file"
-  fi
-
-  rm -f "$tmp_file"
-}
-
 function showman_update() {
-  if [[ -f /tmp/stopfile ]]; then
-    log_action "skip"
-    exit 1
-  fi
   compose_pull 
   compose_up > "$tmp_file" 2>&1
-  update_log
 }
 
 case "$1" in
@@ -223,29 +93,9 @@ case "$1" in
     printf "\n\e[92mBringing up Showman containers...\e[0m\n\n"
     showman_up
     ;;
-  update)
-    printf "\n\e[92mStarting update of Showman containers...\e[0m\n\n"
-    showman_update
-    ;;
-  down)
-    printf "\n\e[91mShutting down Showman containers...\e[0m\n\n"
-    showman_down
-    ;;
-  stop)
-    printf "\n\e[91mStopping Showman containers...\e[0m\n\n"
-    showman_stop
-    ;;
   start)
     printf "\n\e[92mStarting Showman containers...\e[0m\n\n"
     showman_start
-    ;;
-  destroy)
-    printf "\n\e[92mClearing Showman containers...\e[0m\n\n"
-    showman_destroy
-    ;;
-  rotate)
-    printf "\n\e[92mRotating log files...\e[0m\n\n"
-    log_rotate
     ;;
   *)
     printf "\n\e[34mUsage: showman.sh (install|update|stop|start)\e[0m\n\n"
